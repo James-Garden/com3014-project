ac076d56d45c697ce372a2cf1ed2f5b220dcb637
James Garden <jamesegarden@outlook.com>
2024-03-08 16:27:39 +0000

    Initialise User API

---
+++ user-api/src/test/java/uk/ac/surrey/com3014/jg01314/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.surrey.com3014.jg01314;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
