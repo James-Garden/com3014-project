2c2399d4bda28bdb011bacb8dfb2fa11d78a1af1
James Garden <jamesegarden@outlook.com>
2024-03-16 19:45:59 +0000

    Added Session deletion endpoints

--- user-api/src/main/java/uk/ac/surrey/com3014/jg01314/session/SessionService.java
+++ user-api/src/main/java/uk/ac/surrey/com3014/jg01314/session/SessionService.java
@@ -1,5 +1,6 @@
 package uk.ac.surrey.com3014.jg01314.session;
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.ac.surrey.com3014.jg01314.user.User;
@@ -19,4 +20,11 @@ class SessionService {
     return sessionRepository.save(session);
   }
 
+  Optional<Session> findSession(String sessionId) {
+    return sessionRepository.findById(sessionId);
+  }
+
+  public void deleteSession(Session session) {
+    sessionRepository.delete(session);
+  }
 }
