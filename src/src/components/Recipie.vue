29081120b401f9fdc1fabb616d80e0b37f913e6e
giahuy103202 <lamgiahuy1003@hotmail.com>
2024-03-27 19:00:33 +0000

    update

--- src/src/components/Recipie.vue
+++ src/src/components/Recipie.vue
@@ -41,7 +41,7 @@
           id="password"
           class="!w-[914px] !h-[49px] rounded-md pl-5"
           v-model="password"
-          placeholder="Search by Ingredient"
+          placeholder="Search by Recipe"
         />
       </div>
       <div
